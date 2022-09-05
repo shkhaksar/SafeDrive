62e8334f716472cb59cee60ce6420d40fae5141c
Shayan Khaksar <sh.khaksar95@gmail.com>
2022-09-05 15:44:33 +0200

    Added Face Detection and Camera.

---
+++ app/src/main/java/sh/khaksar/safedrive/CameraXViewModel.kt
@@ -0,0 +1,42 @@
+package sh.khaksar.safedrive
+
+import android.app.Application
+import android.util.Log
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutionException
+
+/** View model for interacting with CameraX. */
+class CameraXViewModel(application: Application) : AndroidViewModel(application) {
+    private var cameraProviderLiveData: MutableLiveData<ProcessCameraProvider>? = null
+
+    // Handle any errors (including cancellation) here.
+    val processCameraProvider: MutableLiveData<ProcessCameraProvider>?
+        get() {
+            if (cameraProviderLiveData == null) {
+                cameraProviderLiveData = MutableLiveData<ProcessCameraProvider>()
+                val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> = ProcessCameraProvider.getInstance(getApplication())
+                cameraProviderFuture.addListener(
+                    {
+                        try {
+                            cameraProviderLiveData!!.setValue(cameraProviderFuture.get())
+                        } catch (e: ExecutionException) {
+                            // Handle any errors (including cancellation) here.
+                            Log.e(TAG, "Unhandled exception", e)
+                        } catch (e: InterruptedException) {
+                            Log.e(TAG, "Unhandled exception", e)
+                        }
+                    },
+                    ContextCompat.getMainExecutor(getApplication())
+                )
+            }
+            return cameraProviderLiveData
+        }
+
+    companion object {
+        private const val TAG = "CameraXViewModel"
+    }
+}
--- app/src/main/java/sh/khaksar/safedrive/MainActivity.kt
+++ app/src/main/java/sh/khaksar/safedrive/MainActivity.kt
@@ -1,11 +1,237 @@
 package sh.khaksar.safedrive
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.Surface.ROTATION_180
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.vision.demo.kotlin.facedetector.FaceDetectorProcessor
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import sh.khaksar.safedrive.preference.PreferenceUtils
 
 class MainActivity : AppCompatActivity() {
+
+    private var previewView: PreviewView? = null
+    private var graphicOverlay: GraphicOverlay? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var previewUseCase: Preview? = null
+    private var analysisUseCase: ImageAnalysis? = null
+    private var imageProcessor: VisionImageProcessor? = null
+    private var needUpdateGraphicOverlayImageSourceInfo = false
+    private var lensFacing = CameraSelector.LENS_FACING_FRONT
+    private var cameraSelector: CameraSelector? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+
+        if (!isRuntimePermissionsGranted()) {
+            getRuntimePermissions()
+        }
+
+        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
         setContentView(R.layout.activity_main)
+        previewView = findViewById(R.id.preview_view)
+        if (previewView == null) {
+            Log.d(TAG, "previewView is null")
+        }
+
+        graphicOverlay = findViewById(R.id.graphic_overlay)
+        if (graphicOverlay == null) {
+            Log.d(TAG, "graphicOverlay is null")
+        }
+
+
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[CameraXViewModel::class.java]
+            .processCameraProvider
+            ?.observe(
+                this,
+                Observer { provider: ProcessCameraProvider? ->
+                    cameraProvider = provider
+                    bindAllCameraUseCases()
+                }
+            )
+
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        bindAllCameraUseCases()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        imageProcessor?.run { this.stop() }
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        imageProcessor?.run { this.stop() }
+    }
+
+    private fun bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            // As required by CameraX API, unbinds all use cases before trying to re-bind any of them.
+            cameraProvider!!.unbindAll()
+            bindPreviewUseCase()
+            bindAnalysisUseCase()
+        }
+    }
+
+    private fun bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return
+        }
+        if (previewUseCase != null) {
+            cameraProvider!!.unbind(previewUseCase)
+        }
+
+        val builder = Preview.Builder()
+        builder.setTargetRotation(ROTATION_180)
+        previewUseCase = builder.build()
+        previewUseCase!!.setSurfaceProvider(previewView!!.surfaceProvider)
+        cameraProvider!!.bindToLifecycle(/* lifecycleOwner= */ this,
+            cameraSelector!!,
+            previewUseCase
+        )
+    }
+
+    private fun bindAnalysisUseCase() {
+        if (cameraProvider == null) {
+            return
+        }
+        if (analysisUseCase != null) {
+            cameraProvider!!.unbind(analysisUseCase)
+        }
+        if (imageProcessor != null) {
+            imageProcessor!!.stop()
+        }
+
+        val faceDetectorOptions = FaceDetectorOptions.Builder()
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+            .setMinFaceSize(0.1f)
+            .enableTracking()
+
+        imageProcessor = FaceDetectorProcessor(this, faceDetectorOptions.build())
+
+        val builder = ImageAnalysis.Builder()
+        val targetResolution = PreferenceUtils.getCameraXTargetResolution(this, lensFacing)
+        if (targetResolution != null) {
+            builder.setTargetResolution(targetResolution)
+        }
+        analysisUseCase = builder.build()
+
+        needUpdateGraphicOverlayImageSourceInfo = true
+
+        analysisUseCase?.setAnalyzer(
+            // imageProcessor.processImageProxy will use another thread to run the detection underneath,
+            // thus we can just runs the analyzer itself on main thread.
+            ContextCompat.getMainExecutor(this),
+            ImageAnalysis.Analyzer { imageProxy: ImageProxy ->
+                if (needUpdateGraphicOverlayImageSourceInfo) {
+                    val isImageFlipped = lensFacing == CameraSelector.LENS_FACING_FRONT
+                    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                    if (rotationDegrees == 0 || rotationDegrees == 180) {
+                        graphicOverlay!!.setImageSourceInfo(
+                            imageProxy.width,
+                            imageProxy.height,
+                            isImageFlipped
+                        )
+                    } else {
+                        graphicOverlay!!.setImageSourceInfo(
+                            imageProxy.height,
+                            imageProxy.width,
+                            isImageFlipped
+                        )
+                    }
+                    needUpdateGraphicOverlayImageSourceInfo = false
+                }
+                try {
+                    imageProcessor!!.processImageProxy(imageProxy, graphicOverlay)
+                } catch (e: MlKitException) {
+                    Log.e(TAG, "Failed to process image. Error: " + e.localizedMessage)
+                    Toast.makeText(applicationContext, e.localizedMessage, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        )
+        cameraProvider!!.bindToLifecycle(/* lifecycleOwner= */ this,
+            cameraSelector!!,
+            analysisUseCase
+        )
+    }
+
+    private fun isRuntimePermissionsGranted(): Boolean {
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission.let {
+                if (!isPermissionGranted(this, it)) {
+                    permissionsToRequest.add(permission)
+                }
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i(TAG, "Permission granted: $permission")
+            return true
+        }
+        Log.i(TAG, "Permission NOT granted: $permission")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "sh.khaksar.safedrive.MainActivity"
+        private const val PERMISSION_REQUESTS = 1
+
+        private val REQUIRED_RUNTIME_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA
+        )
     }
 }
